d80024c78c3ff76905e69765ca5ccfeca76b45ac
robwizzie <robertwiscount@gmail.com>
2024-02-23 20:49:24 -0500

    Updates to TTB Site

--- Navbar/navbar.js
+++
@@ -1,5 +0,0 @@
-async function loadNavbar() {
-    let response = await fetch('../Navbar/navbar.html');
-    let content = await response.text();
-    document.getElementById('navbar-placeholder').innerHTML = content;
-}
